e9c9505d2b86750a6aba4337c052409c96a29918
William Pratt <willpratt78@gmail.com>
2022-04-22 16:31:32 -0700

    copied the only code I was able to recover after computer crashed

---
+++ src/exchanger.js
@@ -0,0 +1,13 @@
+export class CurrencyExchanger {
+  static amount(input) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`) 
+    .then(function(response) {
+      if(!response.ok) {
+        throw Error(response.statusText);
+      }else return response.json();
+    })
+    .catch(function(error) {
+      return error;
+    })
+    }
+}
