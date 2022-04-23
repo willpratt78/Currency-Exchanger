33f28463b8dcc577985c852395500d23979268d6
William Pratt <willpratt78@gmail.com>
2022-04-22 20:40:14 -0700

    add UI logic to have results post to DOM

--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import { CurrencyExchanger } from '../exchanger.js';
+import { CurrencyExchanger } from '../src/exchanger.js';
+
+
+function getElements(response,currency1, currency2, amount) {
+  if (response.result) {
+    $("#output").text(`The exchange rate for ${currency1} to ${currency2} is ${amount}`);
+  }else {
+    $("#output").text(`there was an error: ${response.message}`);
+  }
+}
 
 $(document).ready(function() {
   $("#button").on("click", function() {
     let currency1 = $("#currency1").val();
     let currency2 = $("#currency2").val();
-  })
-})
+    let amount = $("#amount").val();
+
+    CurrencyExchanger.amount(currency1, currency2, amount)
+      .then(function(response) {
+        getElements(response, currency1, currency2, amount);
+      });
+  });
+});
