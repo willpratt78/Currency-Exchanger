55d6a5d773b397b39e8bb46b9820ac25989a60e7
William Pratt <willpratt78@gmail.com>
2022-04-22 20:03:39 -0700

    update HTMl, start UI logic

--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { CurrencyExchanger } from '../exchanger.js';
+
+$(document).ready(function() {
+  $("#button").on("click", function() {
+    let currency1 = $("#currency1").val();
+    let currency2 = $("#currency2").val();
+  })
+})
